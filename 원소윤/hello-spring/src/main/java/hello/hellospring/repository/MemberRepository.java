f07bbeef6b79002355d13dcb1b088f13d7a7ffab
Soyun Won <soyun9902@ewhain.net>
2022-04-04 23:15:12 +0900

    섹션 3. 회원 도메인과 리포지토리 만들기

---
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
