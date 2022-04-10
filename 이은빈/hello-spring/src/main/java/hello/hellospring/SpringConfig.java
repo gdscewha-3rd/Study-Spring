6ccc11762806f94fbb3ddbdf2ba58354fb43af09
binable43 <eb.eunbinlee@gmail.com>
2022-04-10 22:14:27 +0900

    6-3.스프링 통합테스트

--- 이은빈/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 이은빈/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource datasource) {
+        this.dataSource = datasource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -15,7 +26,10 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository()
+    {
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 }
