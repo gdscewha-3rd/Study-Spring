68d0446b81f22362eaa14976ec69e0e6bc47facc
binable43 <eb.eunbinlee@gmail.com>
2022-05-04 11:21:47 +0900

    lab 7.AOP

--- 이은빈/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 이은빈/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,4 +1,5 @@
 package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository() {
     //return new MemoryMemberRepository();
--- 이은빈/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 이은빈/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -21,10 +21,19 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        //같은 이름이 있는 경우 회원가입 불가
-        validateDuplicateMember(member);  //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            //같은 이름이 있는 경우 회원가입 불가
+            validateDuplicateMember(member);  //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -38,7 +47,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
