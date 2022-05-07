a2a5954e0e503a5b7c9d1daa595d7bca2ad777c6
Soyun Won <soyun9902@ewhain.net>
2022-05-08 07:15:50 +0900

    섹션 4. 컴포넌트 스캔과 자동 의존관계 설정

--- 원소윤/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>(); //save 시 저장할 메모리
