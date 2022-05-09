3050b43739146b28bb094b9151ac497f859c6baf
Soyun Won <soyun9902@ewhain.net>
2022-05-10 00:31:15 +0900

    섹션 5. 회원 웹 기능 - 등록

--- 원소윤/hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    //정적 파일보다 controller 우선순위가 높음
     @GetMapping("/")
         public String home() {
             return "home";
--- 원소윤/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") //get은 주로 조회할 때 사용
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") //데이터 넣어서 전달할 때 post 사용
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
