27278275e4edac34d7c28879886123ba42c5c6ef
Soyun Won <soyun9902@ewhain.net>
2022-05-15 02:28:01 +0900

    섹션 7. AOP 적용 + SpringDataJpaMemberRepository 위치 수정

--- 원소윤/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
@@ -22,6 +23,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
     //@Bean
     //public MemberRepository memberRepository(){
