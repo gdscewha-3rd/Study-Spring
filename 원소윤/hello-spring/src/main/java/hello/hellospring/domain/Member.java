bab64184b52a952ac68638e4c07481901e7bd1de
Soyun Won <soyun9902@ewhain.net>
2022-05-14 23:54:06 +0900

    섹션 6. JPA

--- 원소윤/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    //pk
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username")
     private String name;
 
     public Long getId() {
---
+++ 원소윤/hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
