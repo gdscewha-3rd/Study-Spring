9ed3624b13ac72b2e4b10917b5462356e1667225
Soyun Won <soyun9902@ewhain.net>
2022-05-25 02:52:29 +0900

    섹션 3. 새로운 구조와 할인 정책 적용

--- 원소윤/core/src/main/java/hello/core/AppConfig.java
+++ 원소윤/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-     public OrderService orderService(){
-            return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+            return new OrderServiceImpl(memberRepository(), discountPolicy());
+     }
+
+     public DiscountPolicy discountPolicy(){ //메소드명에서 역할이 보임
+        return new FixDiscountPolicy();
      }
 }
