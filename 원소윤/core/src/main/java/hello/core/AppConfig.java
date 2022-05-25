3dce6d6396f21b486c25405e8b53f05da31ad442
Soyun Won <soyun9902@ewhain.net>
2022-05-25 20:02:42 +0900

    섹션 3. 스프링으로 전환하기

--- 원소윤/core/src/main/java/hello/core/AppConfig.java
+++ 원소윤/core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //애플리케이션의 설정 정보
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
             return new OrderServiceImpl(memberRepository(), discountPolicy());
      }
 
+    @Bean
      public DiscountPolicy discountPolicy(){ //메소드명에서 역할이 보임
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
