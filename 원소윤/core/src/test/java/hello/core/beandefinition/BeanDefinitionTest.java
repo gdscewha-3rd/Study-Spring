144397dd5b7026d9c5edfca126cba6999d345a8f
Soyun Won <soyun9902@ewhain.net>
2022-06-05 00:03:34 +0900

    섹션 4. 스프링 빈 설정 메타 정보 - BeanDefinition

--- 원소윤/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ 원소윤/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+    // 스프링에 빈을 등록하는 방법 중 잘 쓰는 2가지
+    // 1. 직접 스트링 빈을 등록하는 방법
+    // 2. 팩토리 메소드를 쓰는 방법 = 우리가 자바 코드로 쓴 방법
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
